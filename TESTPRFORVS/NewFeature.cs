414201538063eb3b37278c78c874562c4579c8a7
roochaga <roochaga@WKWEJ754963407>
2026-04-20 13:25:47 +0530

    Add new feature implementation with welcome message and sum calculation

---
+++ TESTPRFORVS/NewFeature.cs
@@ -0,0 +1,17 @@
+
+// New feature implementation
+using System;
+namespace TestPRForVisualStudio
+{
+    public class NewFeature
+    {
+        public string GetWelcomeMessage()
+        {
+            return "Welcome to the new feature!";
+        }
+        public int CalculateSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
